38d5ab8837e5d4beb13dc04eb805f7fcb3b56e27
Niall Carroll <niallpcarroll@gmail.com>
2024-11-08 15:59:23 +0000

    Add post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -91,6 +91,11 @@ DATABASES = {
 
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
