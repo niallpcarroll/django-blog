fb56577423b6b703330263a73f5d5952103a0b51
Niall Carroll <niallpcarroll@gmail.com>
2024-11-14 20:00:09 +0000

    Update urls.py, index.html, and views.py

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,13 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status="1")
+    queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
 
 def post_detail(request, slug):
     """
@@ -17,14 +20,10 @@ def post_detail(request, slug):
     ``post``
         An instance of :model:`blog.Post`.
 
-    **Template**
+    **Template:**
 
-    :template:`blog/post_detail.html`    
-    """    
-
+    :template:`blog/post_detail.html`
+    """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request, "blog/post_detail.html", {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
